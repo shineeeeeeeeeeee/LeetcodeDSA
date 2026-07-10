9a646316629ddfa23652df27058874846fec1b01
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-07-10 07:00:16 +0530

    Time: 4 ms (98.46%) | Memory: 82.8 MB (80.84%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int maxCost = 0;
+
+        for (int cost : costs) {
+            maxCost = Math.max(maxCost, cost);
+        }
+
+        int[] freq = new int[maxCost + 1];
+
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+
+        int ans = 0;
+
+        for (int cost = 1; cost <= maxCost; cost++) {
+            if (freq[cost] == 0) continue;
+
+            int canBuy = Math.min(freq[cost], coins / cost);
+            ans += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) break;
+        }
+
+        return ans;
+    }
+}
