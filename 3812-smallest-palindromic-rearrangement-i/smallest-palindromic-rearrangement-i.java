593200d5b466c91843674276f858628ee3239566
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-07-28 06:26:37 +0530

    Time: 37 ms (20.48%) | Memory: 48.6 MB (10.04%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        StringBuilder left = new StringBuilder();
+        String middle = "";
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < freq[i] / 2; j++) {
+                left.append((char) ('a' + i));
+            }
+
+            if (freq[i] % 2 == 1) {
+                middle = String.valueOf((char) ('a' + i));
+            }
+        }
+
+        String right = left.reverse().toString();
+        left.reverse();
+
+        return left.toString() + middle + right;
+    }
+}
