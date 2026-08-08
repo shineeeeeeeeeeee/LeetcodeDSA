5ec38983ce49e46de3e1cc8badb69569dd96013a
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-08-08 09:39:01 +0530

    Time: 24 ms (97.22%) | Memory: 144.2 MB (13.89%) - LeetSync

---
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        char[] a = word1.toCharArray();
+        char[] b = word2.toCharArray();
+
+        // dp[i] = number of characters of word2 that can be
+        // matched exactly using word1[i...n-1]
+        int[] dp = new int[n + 1];
+
+        int j = m - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1];
+
+            if (j >= 0 && a[i] == b[j]) {
+                dp[i]++;
+                j--;
+            }
+        }
+
+        int[] ans = new int[m];
+
+        int i = 0;
+        j = 0;
+        boolean usedMismatch = false;
+
+        while (i < n && j < m) {
+
+            // Normal exact match
+            if (a[i] == b[j]) {
+                ans[j] = i;
+                j++;
+            }
+
+            // Use this position as the one allowed mismatch
+            else if (!usedMismatch &&
+                     dp[i + 1] >= m - j - 1) {
+
+                ans[j] = i;
+                j++;
+                usedMismatch = true;
+            }
+
+            i++;
+        }
+
+        // Couldn't match all characters
+        if (j < m) {
+            return new int[0];
+        }
+
+        return ans;
+    }
+}
