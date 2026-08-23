8da6a03a6c707b19534bc5a1d8bb9da44663c360
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-08-23 08:25:47 +0530

    Time: 6 ms (97.08%) | Memory: 47.3 MB (10.95%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        return diff * 2 + qDiff * 9 != 0;
+    }
+}
