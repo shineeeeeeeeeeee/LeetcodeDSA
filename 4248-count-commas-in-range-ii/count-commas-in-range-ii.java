804c61e242309fe600ab50b700efedf122238e66
Shine Suri <139244550+shineeeeeeeeeeee@users.noreply.github.com>
2026-09-09 07:44:55 +0530

    Time: 1 ms (99.36%) | Memory: 41.9 MB (99.36%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+        long p = 1000;
+
+        while (p <= n) {
+            ans += n - p + 1;
+            p *= 1000;
+        }
+
+        return ans;
+    }
+}
